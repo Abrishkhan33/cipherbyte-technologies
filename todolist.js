bfdf1f355f30e29c0e6cef56b819113a07176438
Abrishkhan33 <133768075+Abrishkhan33@users.noreply.github.com>
2024-04-16 11:49:25 -0700

    Task 3 to do list

---
+++ todolist.js
@@ -0,0 +1,30 @@
+function addtodo(){
+     var todolist = document.getElementById("todolist");
+
+     var todoinput = document.getElementById("todoinput").value;
+
+     
+     if(todoinput == ""){
+
+        document.getElementById("error").innerText = "Please fill input box !"
+        
+        todo.innerHTML="";
+
+     }else{
+        document.getElementById("error").innerText = ""
+
+        var todo = document.createElement("li");
+
+        todo.innerHTML=(todoinput);
+   
+        todolist.append(todo);
+
+        document.getElementById("todoinput").value = "";
+     }
+
+
+}
+
+
+
+
